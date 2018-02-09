5e746c36a150fdad438641b71f2b2f57e6dbff45
Cobber <codecobber@gmail.com>
2018-02-09 11:57:50 +0000

    Add files via upload

---
+++ rental/index.php
@@ -0,0 +1,243 @@
+<?php include('includes/header.inc.php'); ?><div id='start'>
+
+
+       <div id="bck_1" class="bck"><div id="block_1" class="row rowNo05">
+
+<div class="medium-6 columns medium-push-6">
+<img class="imgPlace" src="http://placehold.it/750x350">
+</div>
+<div class="medium-6 columns medium-pull-6 ">
+<h2>Our Agency, our selves.</h2>
+<p>Vivamus luctus urna sed urna ultricies ac tempor dui sagittis. In condimentum facilisis porta. Sed nec diam eu diam mattis viverra. Nulla fringilla, orci ac euismod semper, magna diam porttitor mauris, quis sollicitudin sapien justo in libero. Vestibulum mollis mauris enim. Morbi euismod magna ac lorem rutrum elementum. Donec viverra auctor.</p>
+</div>
+
+</div></div><div id="bck_2" class="bck"><div id="block_2" class="row rowNo15">
+  <div class="medium-6 columns">
+    <h3>
+      Contact Me
+    </h3>
+    <p>Vivamus hendrerit arcu sed erat molestie vehicula. Sed auctor neque eu tellus rhoncus ut eleifend nibh porttitor. Ut in nulla enim. Phasellus molestie magna non est bibendum non venenatis nisl tempor. Suspendisse dictum feugiat nisl ut dapibus. Mauris iaculis porttitor.</p>
+    <ul class="menu">
+      <li>
+        <a href="#">Dribbble</a>
+      </li>
+      <li>
+        <a href="#">Facebook</a>
+      </li>
+      <li>
+        <a href="#">Yo</a>
+      </li>
+    </ul>
+  </div>
+  <div class="medium-6 columns">
+    <label>
+      Name
+
+      <input type="text" placeholder="Name">
+    </label>
+    <label>
+      Email
+
+      <input type="text" placeholder="Email">
+    </label>
+    <label>
+
+                Message
+
+      <textarea placeholder="holla at a designerd"></textarea>
+    </label>
+    <input type="submit" class="button expanded" value="Submit">
+  </div>
+</div></div><div id="bck_3" class="bck"><div id="block_3" class="row rowNo22">
+    <div class="large-6 columns">
+        <div class="row small-up-2 medium-up-4 large-up-4">
+            <div class="column">
+            <img class="imgPlace  thumbnail" src="http://placehold.it/102x102">
+            </div>
+            <div class="column">
+            <img class="imgPlace  thumbnail" src="http://placehold.it/102x102">
+            </div>
+            <div class="column">
+            <img class="imgPlace  thumbnail" src="http://placehold.it/102x102">
+            </div>
+            <div class="column">
+            <img class="imgPlace  thumbnail" src="http://placehold.it/102x102">
+            </div>
+            <div class="column">
+            <img class="imgPlace  thumbnail" src="http://placehold.it/102x102">
+            </div>
+            <div class="column">
+            <img class="imgPlace  thumbnail" src="http://placehold.it/102x102">
+            </div>
+            <div class="column">
+            <img class="imgPlace  thumbnail" src="http://placehold.it/102x102">
+            </div>
+            <div class="column">
+            <img class="imgPlace  thumbnail" src="http://placehold.it/102x102">
+            </div>
+        </div>
+    </div>
+
+    <div class="large-6 columns">
+        <div class="panel media" id="mediaPodLink">
+            <h4>Media - making life easier</h4>
+            <p>From physio help videos to audio translations and downloadable literature, take at look at our range of materials in a variety of formats.  </p>
+            <a href="medialibrary.php" class="button round">Go to media library</a>
+        </div>
+    </div>
+
+
+</div></div><div id="bck_4" class="bck"><div id="block_4" class="row rowNo16"><div class="medium-8 columns">
+
+
+<div class="row">
+<div class="large-6 columns">
+<p><img class="imgPlace" src="http://placehold.it/600x170&text=Look at me!" alt="image for article"></p>
+</div>
+<div class="large-6 columns">
+<h5><a href="#">'Death Star' Vaporizes Its Own Planet</a></h5>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae impedit beatae, ipsum delectus aperiam nesciunt magni facilis ullam.</p>
+</div>
+</div>
+<div class="row">
+<div class="large-6 columns">
+<p><img class="imgPlace" src="http://placehold.it/600x170&text=Look at me!" alt="image for article"></p>
+</div>
+<div class="large-6 columns">
+<h5><a href="#">'Death Star' Vaporizes Its Own Planet</a></h5>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae impedit beatae, ipsum delectus aperiam nesciunt magni facilis ullam.</p>
+</div>
+</div>
+  <div class="row">
+<div class="large-6 columns">
+<p><img class="imgPlace" src="http://placehold.it/600x170&text=Look at me!" alt="image for article"></p>
+</div>
+<div class="large-6 columns">
+<h5><a href="#">'Death Star' Vaporizes Its Own Planet</a></h5>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae impedit beatae, ipsum delectus aperiam nesciunt magni facilis ullam.</p>
+</div>
+</div></div>
+<div class="medium-4 columns">
+<p><img class="imgPlace" src="http://placehold.it/400x185&text=Other cool article" alt="article promo image"></p>
+<p><img class="imgPlace" src="http://placehold.it/400x185&text=Other cool article" alt="article promo image"></p>
+</div>
+</div></div><div id="bck_5" class="bck"><div id="block_5" class="row rowNo16"><div class="medium-8 columns">
+
+
+<div class="row">
+<div class="large-6 columns">
+<p><img class="imgPlace" src="http://placehold.it/600x170&text=Look at me!" alt="image for article"></p>
+</div>
+<div class="large-6 columns">
+<h5><a href="#">'Death Star' Vaporizes Its Own Planet</a></h5>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae impedit beatae, ipsum delectus aperiam nesciunt magni facilis ullam.</p>
+</div>
+</div>
+<div class="row">
+<div class="large-6 columns">
+<p><img class="imgPlace" src="http://placehold.it/600x170&text=Look at me!" alt="image for article"></p>
+</div>
+<div class="large-6 columns">
+<h5><a href="#">'Death Star' Vaporizes Its Own Planet</a></h5>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae impedit beatae, ipsum delectus aperiam nesciunt magni facilis ullam.</p>
+</div>
+</div>
+  <div class="row">
+<div class="large-6 columns">
+<p><img class="imgPlace" src="http://placehold.it/600x170&text=Look at me!" alt="image for article"></p>
+</div>
+<div class="large-6 columns">
+<h5><a href="#">'Death Star' Vaporizes Its Own Planet</a></h5>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae impedit beatae, ipsum delectus aperiam nesciunt magni facilis ullam.</p>
+</div>
+</div></div>
+<div class="medium-4 columns">
+<p><img class="imgPlace" src="http://placehold.it/400x185&text=Other cool article" alt="article promo image"></p>
+<p><img class="imgPlace" src="http://placehold.it/400x185&text=Other cool article" alt="article promo image"></p>
+</div>
+</div></div><div id="bck_6" class="bck"><div id="block_6" class="row columns rowNo32">
+<header id="header">
+	<h2>Another headline that is not really that interesting</h2>
+	<ul class="icons">
+       <li><a href="#"><i class="fa fa-twitter fa-lg" aria-hidden="true"></i></a></li>
+       <li><a href="#"><i class="fa fa-facebook fa-lg" aria-hidden="true"></i></a></li>
+       <li><a href="#"><i class="fa fa-pinterest-p fa-lg" aria-hidden="true"></i></a></li>
+    </ul>
+</header>
+</div></div><div id="bck_7" class="bck"><div id="block_7" class="row rowNo33">
+
+
+<div class="medium-4 columns">
+<h3>Photoshop</h3>
+<p>Vivamus luctus urna sed urna ultricies ac tempor dui sagittis. In condimentum facilisis porta. Sed nec diam eu diam mattis viverra. Nulla fringilla, orci ac euismod semper, magna.</p>
+ <a class="button" href="#">So Basic</a>
+</div>
+<div class="medium-4 columns">
+<h3>Javascript</h3>
+<p>Vivamus luctus urna sed urna ultricies ac tempor dui sagittis. In condimentum facilisis porta. Sed nec diam eu diam mattis viverra. Nulla fringilla, orci ac euismod semper, magna.</p>
+<a class="button" href="#">So Basic</a>
+</div>
+<div class="medium-4 columns">
+<h3>Marketing</h3>
+<p>Vivamus luctus urna sed urna ultricies ac tempor dui sagittis. In condimentum facilisis porta. Sed nec diam eu diam mattis viverra. Nulla fringilla, orci ac euismod semper, magna.</p>
+<a class="button" href="#">So Basic</a>
+</div>
+</div></div><div id="bck_8" class="bck"><div id="block_8" class="row rowNo06">
+
+
+<div class="medium-4 columns">
+<h3>Photoshop</h3>
+<p>Vivamus luctus urna sed urna ultricies ac tempor dui sagittis. In condimentum facilisis porta. Sed nec diam eu diam mattis viverra. Nulla fringilla, orci ac euismod semper, magna.</p>
+</div>
+<div class="medium-4 columns">
+<h3>Javascript</h3>
+<p>Vivamus luctus urna sed urna ultricies ac tempor dui sagittis. In condimentum facilisis porta. Sed nec diam eu diam mattis viverra. Nulla fringilla, orci ac euismod semper, magna.</p>
+</div>
+<div class="medium-4 columns">
+<h3>Marketing</h3>
+<p>Vivamus luctus urna sed urna ultricies ac tempor dui sagittis. In condimentum facilisis porta. Sed nec diam eu diam mattis viverra. Nulla fringilla, orci ac euismod semper, magna.</p>
+</div>
+</div></div><div id="bck_9" class="bck"><div id="block_9" class="row rowNo02">
+<div class="medium-8 columns">
+
+		<figure class="effImg">
+			<img class="imgPlace" src="http://placehold.it/900x450&text=Other cool article" alt="article promo image">
+		</figure>
+
+</div>
+<div class="medium-4 columns">
+
+		<figure class="effImg">
+			<img class="imgPlace" src="http://placehold.it/400x200&text=Other cool article" alt="article promo image">
+		</figure>
+
+		<figure class="effImg">
+			<img class="imgPlace" src="http://placehold.it/400x200&text=Other cool article" alt="article promo image">
+		</figure>
+
+</div>
+</div></div><div id="bck_10" class="bck"><div id="block_10" class="row rowNo29">               <div class="large-6 columns shadingAdpod">
+                    <div class="panel">
+                    <h5>Contemporary Art and Crafts in Biggar</h5>
+                      <p>
+                        The Gladstone Gallery is a contemporary art and craft gallery based in the historic town of Biggar.
+                        Opened in Summer 2011, we are proud to present a varied selection of works by top quality artists from around Scotland and the North of England.
+                     </p>
+                     <h5>Newest Arrivals</h5>
+                    <p>Take a look below at some of the new arrivals in the gallery.</p>
+                    </div>
+
+               </div>
+              <div class="large-6 columns shadingAdpod">
+                    <div class="panel news">
+                    <h5>Newsletter Signup</h5>
+                    <p>Sign up for our newsletter today. Don' miss out, get the news and updates from the gallery.</p>
+                      <form>
+                       <fieldset>
+                        <label>Ener your email address</label>
+                        <input id="email" maxlength="25" type="text" name="news">
+                        <input type="submit" value="Sign me up">
+                       </fieldset>
+                      </form>
+                    </div>
+               </div></div></div></div><?php include('includes/footer.inc.php'); ?>
